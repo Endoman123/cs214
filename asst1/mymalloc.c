92af76da5faf343a75dc4071425e97227b613976
Jared Tulayan <jet163@scarletmail.rutgers.edu>
2019-10-13 16:23:17 -0400

    Rename metadata typedef

--- asst1/mymalloc.c
+++ asst1/mymalloc.c
@@ -12,7 +12,7 @@ void *mymalloc(int size, char *file, int nLine) {
     void *ret = NULL;
     
     // Check remaining data
-
+    
     return ret; 
 }
 
@@ -27,22 +27,6 @@ int myfree(void *pointer, char *file, int nLine) {
 }
 
 /**
- * Given the bytes and offset, 
- * sets the bit to the specified value.
- */
-unsigned long setBit(unsigned long bytes, int value, int offset) {
-    return (unsigned long)((bytes & ~(1 << offset)) | (value << offset));
-}
-
-/**
- * Given the bytes and offset,
- * gets width amount of bits.
- */
-unsigned long getBits(unsigned long bytes, int width, int offset) {
-    return (unsigned long)((bytes >> offset) & (unsigned long)(pow(2, width) - 1));
-}
-
-/**
  * Metadata struct using bit fields.
  * This takes up exactly 2 bytes or 16 bits of space. 
  */ 
@@ -50,4 +34,4 @@ typedef struct _metadata_ {
     unsigned short blocksize : 12;
     unsigned short in_use : 1;
     unsigned short identifier : 3;
-} _metadata_;
+} Metadata;
