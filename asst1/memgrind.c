1a30630dd00d6dec5fe5eb3b593139b67f00b174
Jared Tulayan <jet163@scarletmail.rutgers.edu>
2019-10-13 21:25:35 -0400

    Add getAverage() function

--- asst1/memgrind.c
+++ asst1/memgrind.c
@@ -3,29 +3,35 @@
 
 #include "mymalloc.h"
 
+double getAverage(int*, int);
+
 int main(int argc, char **argv) {
     // Setup for measuring execution time. 
-    int workloadA[100]; 
-    int workloadB[100];
-    int workloadC[100];
-    int workloadD[100];
-    int workloadE[100];
-    int workloadF[100];
-    
+    int workload[100];
+        
     struct timeval start, end;
 
     // Begin by seeding the random generator
     srand(time(0));
     
     // A: malloc() 1 byte and immediately free it - do this 150 times
+    printf("Workload A: malloc() 1 byte and free() x150\n");
     int i; for (i = 0; i < 100; i++) {
         gettimeofday(&start, NULL);
-        int j; for (j = 0; j < 150; j++) free(malloc(1));
+        
+        int j; 
+        for (j = 0; j < 150; j++) 
+            free(malloc(1));
+        
         gettimeofday(&end, NULL);
-        workloadA[i] = (int) (end.tv_usec - start.tv_usec);
+        
+        workload[i] = (int) (end.tv_usec - start.tv_usec);
     }
+    printf("Average time: %f\n\n", getAverage(workload, 100));
+
 
     // B: malloc() 1 byte, store the pointer in an array - do this 150 times.
+    printf("Workload B: malloc() 1 byte and store pointer in array x150\n");
     int j, k; 
     for (i = 0; i < 100; i++) {
         gettimeofday(&start, NULL);
@@ -38,10 +44,12 @@ int main(int argc, char **argv) {
                 free(buffer[k]);
         }
         gettimeofday(&end, NULL);
-        workloadB[i] = (int) (end.tv_usec - start.tv_usec);
+        workload[i] = (int) (end.tv_usec - start.tv_usec);
     }
+    printf("Average time: %f\n\n", getAverage(workload, 100));
 
     // C: Randomly choose between a 1 byte malloc() or free()ing a 1 byte pointer
+    printf("Workload C: Random malloc and free\n");
     for (i = 0; i < 100; i++) {
         gettimeofday(&start, NULL);
 
@@ -60,10 +68,20 @@ int main(int argc, char **argv) {
 
         gettimeofday(&end, NULL);
 
-        workloadC[i] = (int) (end.tv_usec - start.tv_usec);
+        workload[i] = (int) (end.tv_usec - start.tv_usec);
+    }
+    printf("Average time: %f\n\n", getAverage(workload, 100));
+    
+    return 0;
+}
 
+double getAverage(int *values, int nValues) {
+    double ret = 0;
+    int i = 0;
+
+    for (; i < nValues; i++) {
+        ret += values[i];
     }
 
-
-    return 0;
+    return ret / nValues;
 }
