d79bfe26c77a4afa7a3d1baea542ca9fbd317241
Jared Tulayan <jet163@scarletmail.rutgers.edu>
2019-10-13 21:03:55 -0400

    Fix issues with memgrind.c

--- asst1/memgrind.c
+++ asst1/memgrind.c
@@ -4,16 +4,20 @@
 #include "mymalloc.h"
 
 int main(int argc, char **argv) {
-    //Setup for measuring execution time. 
+    // Setup for measuring execution time. 
     int workloadA[100]; 
     int workloadB[100];
     int workloadC[100];
     int workloadD[100];
     int workloadE[100];
     int workloadF[100];
+    
     struct timeval start, end;
 
-    //A: malloc() 1 byte and immediately free it - do this 150 times
+    // Begin by seeding the random generator
+    srand(time(0));
+    
+    // A: malloc() 1 byte and immediately free it - do this 150 times
     int i; for (i = 0; i < 100; i++) {
         gettimeofday(&start, NULL);
         int j; for (j = 0; j < 150; j++) free(malloc(1));
@@ -21,36 +25,35 @@ int main(int argc, char **argv) {
         workloadA[i] = (int) (end.tv_usec - start.tv_usec);
     }
 
-    //B: malloc() 1 byte, store the pointer in an array - do this 150 times.
+    // B: malloc() 1 byte, store the pointer in an array - do this 150 times.
     int j, k; 
     for (i = 0; i < 100; i++) {
         gettimeofday(&start, NULL);
         for (j = 0; j < 3; j++) { //Repeat the 50 malloc 50 free cycle 3 times for 150.
-            char buffer[50];
-            for (k = 0; k < 50; k++) {
+            char *buffer[50];
+            for (k = 0; k < 50; k++)
                 buffer[k] = malloc(1);
-            }
-            for (k = 0; k < 50; k++) {
+
+            for (k = 0; k < 50; k++)
                 free(buffer[k]);
-            }
         }
         gettimeofday(&end, NULL);
         workloadB[i] = (int) (end.tv_usec - start.tv_usec);
     }
 
-    //C: Randomly choose between a 1 byte malloc() or free()ing a 1 byte pointer
+    // C: Randomly choose between a 1 byte malloc() or free()ing a 1 byte pointer
     for (i = 0; i < 100; i++) {
         gettimeofday(&start, NULL);
 
-
-        char buffer[50];   
+        char *buffer[50];   
         int mallocOperations = 0;
         j = 0;
 
-        //Generate a random number. If even -> malloc. If odd -> free.
-        //Stop after mallocing 50 times.
+        // Generate a random number. If even -> malloc. If odd -> free.
+        // Stop after mallocing 50 times.
         while (mallocOperations < 50) {
-            if (srand(time(0)) % 2 == 0) buffer[j++] = malloc(1);
+            if (rand() % 2 == 0) 
+                buffer[j++] = malloc(1);
             else free(buffer[j--]);
         }
         for (k = 0; k <= j; k++) free(buffer[k]);
@@ -60,5 +63,7 @@ int main(int argc, char **argv) {
         workloadC[i] = (int) (end.tv_usec - start.tv_usec);
 
     }
+
+
     return 0;
 }
