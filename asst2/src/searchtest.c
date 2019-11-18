98c9d703de84b6c8cc9d4c4bcc12cf7817580c7f
Jared Tulayan <jet163@scarletmail.rutgers.edu>
2019-11-17 21:49:24 -0500

    Add time output to searchtest.c

--- asst2/src/searchtest.c
+++ asst2/src/searchtest.c
@@ -4,13 +4,19 @@
 
 int getRandomValue(int, int);
 
+const unsigned int WORKLOAD_ITERATIONS = 100, SECONDS_TO_MICROSECONDS = 1000000;
+
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        printf("Use: searchtest [nElem]");
+    srand(time(0));
+
+    if (argc != 3) {
+        printf("Use: searchtest [nElem] [filename]");
         return -1;
     }
 
+    FILE *fp = fopen(argv[2], "w");
     int nElem, i, *arr;
+    struct timeval start, end;
 
     sscanf(argv[1], "%d", &nElem);
 
@@ -18,6 +24,8 @@ int main(int argc, char** argv) {
     arr = calloc(nElem, sizeof(int));
     for (i = 0; i < nElem; ++i)
         arr[i] = i;
+    
+    fprintf(fp, "%d,", nElem);
 
     // Shuffle 
     for (i = 0; i < nElem; ++i) {
@@ -29,15 +37,25 @@ int main(int argc, char** argv) {
     }
     
     // Search time
+    // Ihadurca here, time to cloud the server with too many tests
     int val = getRandomValue(0, nElem - 1);
-    for (i = 0; i < 100; ++i) {
-        int from = search(arr, val), temp;
+    for (i = 0; i < 10000000; ++i) {
+        int from, to, temp, time;
+
+        gettimeofday(&start, NULL);
+        from = search(arr, val);
+        gettimeofday(&end, NULL);
+        time = (int) ((end.tv_sec * SECONDS_TO_MICROSECONDS + end.tv_usec) - (start.tv_sec * SECONDS_TO_MICROSECONDS + start.tv_usec)); 
+        fprintf(fp, "%d,", time);
 
         to = getRandomValue(0, nElem - 1);
+        
         temp = arr[from];
         arr[from] = arr[to];
         arr[to] = arr[from];
-    } 
+    }
+
+    fclose(f); 
     
     return 0;
 }
