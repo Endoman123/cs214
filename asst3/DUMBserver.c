94f96d18a281f044f02f967a13a8ede7632be351
Sagar A. Patel <sap336@ilab2.cs.rutgers.edu>
2019-11-28 20:19:25 -0500

    Add basic server code.

---
+++ asst3/DUMBserver.c
@@ -0,0 +1,30 @@
+#include <netinet/in.h>
+
+const char* IP_ADDR = "placeholder"
+
+int main(int argc, char* argv[]) {
+    //User input for the server should be a port number.
+    if (argc != 2) {
+        printf("Error: Incorrect number of arguments.\n")
+    }
+    
+    int port = atoi(argv[1]); //Parse the port from the user input
+     
+    //Create server socket
+    int serv_sock = socket(AF_INET, SOCK_STREAM, 0); 
+
+    //Bind socket
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET; //Use IPv4
+    serv_addr.sin_addr.s_addr = inet_addr(IP_ADDR); //Set IP address
+    serv_addr.sin_port = htons(port); //Set port
+    
+    //Listen for client connection
+    listen(serv_socket, 20);
+
+    //Receive connection
+    struct sockaddr_in clnt_addr;
+    socklen_t cltn_addr_size = sizeof(clnt_addr);
+    int clnt_sock = accept(serv_sock, (struct*) &clnt_addr, &clnt_addr_size);
+    
+}
