7e93cfed266868f53d6d6b64356d6d002ec514f0
Sagar A. Patel <sap336@ilab2.cs.rutgers.edu>
2019-11-28 23:10:03 -0500

    Fix com;ile errors in DUMBclient.c

--- asst3/DUMBclient.c
+++ asst3/DUMBclient.c
@@ -13,19 +13,19 @@ int main(int argc, char* argv[]) {
     int port = atoi(argv[2]); //TODO: *breaths in* SIZE CHECK. 
     
     if (port >= 4096) {
-        printf("Error: The port should be above 4096\n")
+        printf("Error: The port should be above 4096\n");
         return 1;
     }
 
     //Create the client socket
-    int socket = socket(AF_INET, SOCK_STREAM, 0);  
+    int sock = socket(AF_INET, SOCK_STREAM, 0);  
     
     struct sockaddr_in serv_addr;
 
     serv_addr.sin_family = AF_INET;     
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // 127.0.0.1 binds to the local address
     serv_addr.sin_port = htons(port);
-    connect(socket, (struct sockaddr*) &serv_addr, sizeof(serv_addr)); 
+    connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)); 
         
     return 0;
 }
