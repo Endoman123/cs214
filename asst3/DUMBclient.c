f41431d6cd6461492dae823ce4711c0e1475fffb
Sagar A. Patel <sap336@ilab3.cs.rutgers.edu>
2019-12-09 20:34:37 -0500

    Finish client.

--- asst3/DUMBclient.c
+++ asst3/DUMBclient.c
@@ -17,19 +17,12 @@ int main(int argc, char* argv[]) {
     }
 
     char* host = argv[1];
-    int port = atoi(argv[2]); //TODO: *breaths in* SIZE CHECK. 
+    char* port = argv[2]; 
     
-    if (port <= 4096) {
+    if (atoi(port) <= 4096) {
         printf("Error: The port should be above 4096.\n");
         return -1;
     }
-
-    //Initialize socket structs 
-    struct sockaddr_in serv_addr = {  
-        .sin_family = AF_INET,     
-        .sin_addr.s_addr = inet_addr("127.0.0.1"), // 127.0.0.1 binds to the local address
-        .sin_port = htons(port)
-    };
 
     struct addrinfo hints = {
         .ai_family = AF_INET,
@@ -39,7 +32,7 @@ int main(int argc, char* argv[]) {
     //Resolve the hostname
     struct addrinfo* info; 
     int addr_error;
-    if ((addr_error = getaddrinfo(host, NULL, &hints, &info)) != 0) {
+    if ((addr_error = getaddrinfo(host, port, &hints, &info)) != 0) {
         printf("Error: Hostname could not be resolved.\n");
         return -1;
     }
@@ -56,11 +49,11 @@ int main(int argc, char* argv[]) {
         return -1;
     }  
    
-    int err, i;
+    int error, i;
     for (i = 0; i < 3; i++) {
         printf("Connecting...\n");
-        err = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-        if (connect < 0) {
+        error = connect(sock, info -> ai_addr, info -> ai_addrlen);
+        if (error < 0) {
             printf("Failed. Attempting again...\n");
         } else {
             break;
@@ -69,7 +62,7 @@ int main(int argc, char* argv[]) {
     freeaddrinfo(info);
 
     //We failed to connect on the last try. Abandon ship.
-    if (err < 0) {
+    if (error < 0) {
         printf("The server failed to connect. Aborting...\n");
         return -1;
     } else {
@@ -77,23 +70,81 @@ int main(int argc, char* argv[]) {
     }
     
     //Now that we're connected to the server, we can send messages.
-    printf("Input messages to send to the server.\n");
     while (1) {
-        printf("-> ");
+        printf("Send a message: ");
 
         char* userInput;
-        scanf("%m[^\n]", userInput); //Read up to the newline character with buffer allocation done for us.
-        //Check if the user send nothing.
-        if (userInput == NULL) {
-            userInput = "\0";
-        } 
-        
-        //Send the message to the server.
-        //We can do some checks for if the user send the correct tags later.
-        //For now just send the message over.
-        send(sock, userInput, strlen(userInput) + 1, 0);
+        scanf("%m[^\n]", &userInput); //Read up to the newline character with buffer allocation done for us.
 
-        //flush the input buffer
-        fflush(stdin);
+        //Check if the user sent nothing.   
+        if (userInput == NULL) userInput = "\0"; 
+
+        //Send the message to the server.
+        send(sock, userInput, strlen(userInput) + 1, 0);   
+        
+        char* servResponse;
+        receiveServerMsg(sock, &servResponse);
+
+        if (servResponse != NULL && servResponse != "") {
+            printf("Server: %s\n", servResponse);
+        } else {
+            printf("Disconnecting from the server...\n");
+            break;
+        }
+        //flush the input buffer.  
+        char ch; while ((ch = getchar()) != '\n');
+
+        free(userInput);
+        //free(servResponse);
     }
+}
+
+//Input the string to receive the server message
+//Return the length of that string.
+int receiveServerMsg(int servSocket, char** msg) {
+    //We dont know how long the message is going to be. 
+    //So we need a dynamically growing buffer to make sure we read all of
+    //the message we receive from the other end
+    //Start with an arbitrary number of bytes
+    //and grow it until we get all of the bytes we need.
+    
+    const int START_LEN = 8;
+    const int MAX_BUFFER_SIZE = 2048;
+
+    char* buffer = malloc(sizeof(char) * START_LEN);
+    int length = START_LEN;
+
+    char* nullTerminator;
+    int bufferOffset = 0, bytesReceived = 0;
+
+    do {
+        //printf("Recv() with arguments %d, %d, %d, %d\n", servSocket, buffer + bufferOffset, length - bufferOffset, 0);
+        bytesReceived = recv(servSocket, buffer + bufferOffset, length - bufferOffset, 0);
+        if (bytesReceived < 0) {
+            printf("Error: The message from the server could not be read\n");
+            *msg = "";
+            return;
+        } else if (bytesReceived == 0) {
+            *msg = "";
+            return;
+        }
+
+        bufferOffset += bytesReceived;
+
+        if ((nullTerminator = memchr(buffer + bufferOffset, '\0', length - bufferOffset)) == NULL && length < MAX_BUFFER_SIZE) {
+            length *= 2;
+            char* tempBuffer = realloc(buffer, length);
+            if (tempBuffer == NULL) {
+                printf("Error: The message from the server could not be read.\n");
+            } else {
+                buffer = tempBuffer;
+            }
+        } else if (length < MAX_BUFFER_SIZE) break; 
+    } while (nullTerminator == NULL);
+
+    //We've reached the end of the server's message.
+    //The buffer is gonna be a bit bigger than the message we received
+    //So lets cut off the end of the buffer and copy the memory over to msg.
+    *msg = calloc(strlen(buffer) + 1, sizeof(char));
+    memcpy(*msg, buffer, strlen(buffer) + 1); 
 }
